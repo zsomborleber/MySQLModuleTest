23a4143a9a94d7f3c16bad844d1b9270847eaef9
Ria <benedek.maria88@gmail.com>
2022-07-08 22:38:42 +0200

    added expected results to tasks

--- tasks/queries.sql
+++ tasks/queries.sql
@@ -1,6 +1,10 @@
 /*
 Lekérdezések feladatsor
 
+A feladatok a `world` adatbázishoz vonatkoznak.
+Ez az adatbázis három táblát tartalmaz: `city`, `country`, `countrylanguage`.
+A phpMyAdmin online felületén a `gigapoli_progmatic` néven található meg.
+
 Kötelezően megoldandó feladatok: 1 - 15.
 A bónusz feladatok nem kötelezőek.
 
@@ -25,6 +29,9 @@ Jó munkát!
 1. feladat (2 pont)
     Melyik évben nyerte el egy ország legkorábban a függetlenségét?
     Írj lekérdezést, amely visszaadja azt az évszámot, amikor az első ország független lett! (IndepYear)
+
+    Elvárt eredmény:
+        -1523
 */
 
 
@@ -34,6 +41,9 @@ Jó munkát!
     Írj lekérdezést, amely visszaadja annak az országnak az összes adatát, amelyik legkorábban nyerte el a függetlenségét!
     A feladatot egy lekérdezésben oldd meg! Azaz nem elfogadható, ha először lekérdezed a legkorábbi függetlenség évszámát,
     azt kimásolod, és beilleszted a lekérdezésbe.
+
+    Elvárt eredmény:
+        CHN Code-ú ország (China)
 */
 
 
@@ -41,6 +51,11 @@ Jó munkát!
 3. feladat (2 pont)
     Melyek azok a városok, amelyeknél a város neve ugyanaz, mint a körzeté?
     Írj lekérdezést, amely visszaadja a városok összes adatát, amelyeknél a név megegyezik a körzet nevével! (District)
+
+    Elvárt eredmény:
+        550 rekord
+        ID szerint növekvő sorrendben az első 5 darab: 2, 3, 8, 11, 34
+        ID szerint növekvő sorrendben az utolsó 3 darab: 4076, 4078, 4079
 */
 
 
@@ -49,6 +64,10 @@ Jó munkát!
     Mely országoknál tartalmazza az államfő neve az 'Ahmad', az 'Ahmed' vagy a 'Hamad' szavakat?
     Írj lekérdezést, amely visszaadja az ország nevét és az államfő nevét, amennyiben az államfő neve tartalmazza
     a fent leírt szavak bármelyikét! (HeadOfState)
+
+    Elvárt eredmény:
+        7 rekord
+        országnevek: Bangladesh, Bahrain, Kuwait, Mauritania, Qatar, Sudan, Sierra Leone
 */
 
 
@@ -57,6 +76,10 @@ Jó munkát!
     Melyek azok az országok, amelyeknél nincs megadva várható életkor, de van lakosságuk?
     Írj lekérdezést, amely visszaadja azoknak az országoknak az összes adatát, ahol nem szerepel várható életkor (LifeExpectancy),
     de nem lakatlanok.
+
+    Elvárt eredmény:
+        10 rekord
+        kontinensek: Oceania (7 db), Europe (2), South America (1)
 */
 
 
@@ -65,6 +88,9 @@ Jó munkát!
     Mely országoknál nagyobb a GNPOld a GNP-nél?
     Írj lekérdezést, amely visszaadja az ország kódját, nevét, a GNP-t és a GNPOld-ot azokról az országokról,
     ahol a GNPOld nagyobb, mint a GNP!
+
+    Elvárt eredmény:
+        63 rekord
 */
 
 
@@ -73,6 +99,9 @@ Jó munkát!
     Mely nyelvek neve végződik úgy, hogy 'ian'?
     Írj lekérdezést, amely visszaadja a nyelvek nevét, ahol a név 'ian'-ra végződik!
     Mindegyik nyelv csak egyszer szerepeljen a találatok között, és rendezd őket név szerint ABC-sorrendbe!
+
+    Elvárt eredmény:
+        29 rekord
 */
 
 
@@ -81,6 +110,11 @@ Jó munkát!
     Hány darab ország szerepel az adatbázisban a különböző európai régiókban?
     Írj lekérdezést, amely visszaadja a régiót és a régióban található országok darabszámát,
     ahol a régióban szerepel az, hogy 'Europe'!
+
+    Elvárt eredmény:
+        Southern Europe: 15
+        Western Europe: 9
+        Eastern Europe 10
 */
 
 
@@ -90,6 +124,11 @@ Jó munkát!
     Írj lekérdezést, amely visszaadja az ország kódját, nevét és a fővárosának a nevét,
     méghozzá az ország neve szerint ABC-sorrendben!
     A találati listában minden ország szerepeljen - még akkor is, ha nincsen fővárosa.
+
+    Elvárt eredmény:
+        239 rekord
+        első országkód: AFG
+        utolsó országkód: ZWE
 */
 
 
@@ -97,6 +136,10 @@ Jó munkát!
 10. feladat (3 pont)
     Melyek azok a városok, amelyeknél a populáció száma pontosan 3 számjegyből áll?
     Írj lekérdezést, amely visszaadja azoknak a városoknak az összes adatát, amelyeknél a lakosság száma pontosan 3 számjegyből áll!
+
+    Elvárt eredmény:
+        10 rekord
+        a városok ID-ja: 61, 62, 1791, 2316, 2317, 2728, 2805, 2806, 3333, 3538
 */
 
 
@@ -106,6 +149,10 @@ Jó munkát!
     Írj lekérdezést, amely visszaadja azoknak a városoknak az összes adatát,
     amelyek valamilyen 'Nordic Countries' régióhoz tartozó országban vannak,
     méghozzá országkód szerint ABC-sorrendben (növekvő), populáció szerint csökkenő sorrendben!
+
+    Elvárt eredmény:
+        35 rekord
+        országkódok, amelyek szerepelnek a listában: DNK, FIN, FRO, ISL, NOR, SJM, SWE
 */
 
 
@@ -116,6 +163,11 @@ Jó munkát!
     Írj lekérdezés, amely visszaadja a régiót és a régióhoz tartozó azon országok darabszámát, amelyekre igaz,
     hogy nincs érték megadva az `IndepYear`-nél, vagy 1500 előtt nyerték el a függetlenségüket!
     A találatokat rendezd a darabszám szerint csökkenő sorrendbe!
+
+    Elvárt eredmény:
+        19 rekord
+        legkisebb: Northern Africa (1)
+        legnagyobb: Caribbean (11)
 */
 
 
@@ -123,6 +175,10 @@ Jó munkát!
 13. feladat (6 pont)
     Melyek azok az országok, amelyekben nem hivatalos nyelvként beszélik az angolt?
     Írj lekérdezést, amely visszaadja az országok összes adatát, amelyekben az angol nem hivatalos nyelv!
+
+    Elvárt eredmény:
+        16 rekord
+        országkódok: ABW, ANT, BHR, BRN, COK, DNK, ISL, JPN, KWT, MAC, MCO, MDV, MYS, NOR, PRI, TTO
 */
 
 
@@ -130,6 +186,10 @@ Jó munkát!
 14. feladat (6 pont)
     Mely országokhoz nem tartozik egy város sem?
     Írj lekérdezést, amely visszaadja azoknak az országoknak az összes adatát, amelyekhez nem tartozik város az adatbázisban!
+
+    Elvárt eredmény:
+        7 rekord
+        országkódok: ATA, ATF, BVT, HMD, IOT, SGS, UMI
 */
 
 
@@ -138,6 +198,9 @@ Jó munkát!
     Melyik országokhoz tartozik nyelv, ahol a nyelvet a lakosság 0%-a beszéli?
     Írj lekérdezést, amely visszaadja az ország nevét, kontinensét, régióját, a nyelv nevét és százalékát, amelyre igaz,
     hogy a nyelv százaléka 0!
+
+    Elvárt eredmény:
+        65 rekord
 */
 
 
@@ -152,6 +215,9 @@ bónusz 1. (4 pont)
     Melyek azok az országok, amelyek kétbetűs országkódja nem a hárombetűs országkód első két betűje?
     Írj lekérdezést, amely visszaadja az ország hárombetűs kódját, kétbetűs kódját és nevét,
     ahol a hárombetűs országkód első két betűje nem azonos a kétbetűs kóddal!
+
+    Elvárt eredmény:
+        88 rekord
 */
 
 
@@ -162,6 +228,9 @@ bónusz 2. (4 pont)
     amelyek a legkorábban függetlenné vált országhoz tartoznak!
     A feladatot egy lekérdezésben oldd meg! Azaz nem elfogadható, ha először lekérdezed az országkódot,
     az kimásolod, és beleilleszted a lekérdezésbe.
+
+    Elvárt eredmény:
+        363 rekord
 */
 
 
@@ -170,6 +239,14 @@ bónusz 3. (4 pont)
     Mekkora a százaléka a legnagyobb százalékban beszélt nyelveknek országonként?
     Írj lekérdezést, amely visszaadja az országkódot és az országban a legnagyobb százalékban beszélt nyelv százalékát!
     A feladatot egy lekérdezésben oldd meg!
+
+    Elvárt eredmény:
+        233 rekord
+        néhány példa:
+            ABW 76.7
+            AFG 52.4
+            AGO 37.2
+            AIA 0.0
 */
 
 
@@ -179,6 +256,14 @@ bónusz 4. (6 pont)
     Írj lekérdezést, amely visszaadja az országkódot, a nyelvet és a százalékot,
     méghozzá országonként kiválasztva a legnagyobb százalékban beszélt nyelvet!
     A feladatot egy lekérdezésben oldd meg!
+
+    Elvárt eredmény:
+        241 rekord (néhány országban holtverseny van - ezért lesz itt több találat, mint a bónusz 3. feladatnál)
+        néhány példa:
+            ABW, Papiamento, 76.7
+            AFG, Pashto, 52.4
+            AGO, Ovimbundu, 37.2
+            AIA, English, 0.0
 */
 
 
@@ -187,5 +272,13 @@ bónusz 5. (6 pont)
     Hány hivatalos és nem hivatalos nyelv tartozik a különböző országokhoz?
     Írj lekérdezést, amely visszaadja az országkódot, a hozzá tartozó hivatalos nyelvek darabszámát
     és a nem hivatalos nyelvek darabszámát!
+
+    Elvárt eredmény:
+        233 rekord
+        néhány példa:
+            ABW, hivatalos: 1, nem hivatalos: 3
+            AFG, hivatalos: 2, nem hivatalos: 3
+            AGO, hivatalos: 0, nem hivatalos: 9
+            AIA, hivatalos: 1, nem hivatalos: 0
 */
 
